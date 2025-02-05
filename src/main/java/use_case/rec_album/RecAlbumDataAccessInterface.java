61a2c32f89c771d63a5ab8fcda7244d1c7591b7a
riena42 <riena.asakawa@mail.utoronto.ca>
2025-02-04 19:38:25 -0500

    Update RecAlbumDataAccessInterface.java
    Pasted album data access

--- src/main/java/use_case/rec_album/RecAlbumDataAccessInterface.java
+++ src/main/java/use_case/rec_album/RecAlbumDataAccessInterface.java
@@ -1,4 +1,29 @@
 package use_case.rec_album;
 
-public interface RecAlbumDataAccessInterface {
+import entity.Album;
+
+import java.util.ArrayList;
+
+/**
+ * DAO for the Recommend Album Use Case.
+ */
+public interface RecAlbumUserDataAccessInterface {
+
+    /**
+     * Sets the album to be stored.
+     * @param album the new Album
+     */
+    void setAlbum(Album album);
+
+    /**
+     * Retrieve the album.
+     * @return the album.
+     */
+    Album getAlbum();
+
+    /**
+     * Get new album releases on Spotify.
+     * @return new album releases
+     */
+    ArrayList<Album> getNewReleases();
 }
