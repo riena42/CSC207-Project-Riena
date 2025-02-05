86b5f14686ca257320ad0ef4b709838dd458dc7b
riena42 <riena.asakawa@mail.utoronto.ca>
2025-02-04 19:37:04 -0500

    Update InMemoryAlbumDataAccessObject.java
    Pasted album data access object

--- src/main/java/data_access/InMemoryAlbumDataAccessObject.java
+++ src/main/java/data_access/InMemoryAlbumDataAccessObject.java
@@ -1,4 +1,39 @@
 package data_access;
 
-public class InMemoryAlbumDataAccessObject {
+import entity.Album;
+
+import interface_adapter.spotify_auth.SpotifyAuthController;
+import use_case.rec_album.RecAlbumUserDataAccessInterface;
+
+import java.util.ArrayList;
+
+/**
+ * In-memory implementation of the DAO for storing album data. This implementation does
+ * NOT persist data between runs of the program.
+ */
+public class InMemoryAlbumDataAccessObject implements RecAlbumUserDataAccessInterface {
+
+    private Album album;
+    private ArrayList<Album> newReleases;
+    private SpotifyAuthController spotifyAuthController;
+
+    public InMemoryAlbumDataAccessObject(SpotifyAuthController spotifyAuthController) {
+        this.newReleases = new ArrayList<>();
+        this.spotifyAuthController = spotifyAuthController;
+    }
+
+    @Override
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+    @Override
+    public Album getAlbum() {
+        return this.album;
+    }
+
+    @Override
+    public ArrayList<Album> getNewReleases() {
+        return spotifyAuthController.getNewReleases();
+    }
 }
