22f70964a754926f1212f2857728988e1efd4fd7
riena42 <riena.asakawa@mail.utoronto.ca>
2025-02-04 19:39:08 -0500

    Update RecAlbumInputBoundary.java
    pasted input boundary

--- src/main/java/use_case/rec_album/RecAlbumInputBoundary.java
+++ src/main/java/use_case/rec_album/RecAlbumInputBoundary.java
@@ -1,4 +1,14 @@
 package use_case.rec_album;
 
+/**
+ * Input boundary for recommending an album.
+ */
 public interface RecAlbumInputBoundary {
+
+    /**
+     * Executes the use case.
+     *
+     * @param recAlbumInputData Input data for the use case.
+     */
+    void execute(RecAlbumInputData recAlbumInputData);
 }
