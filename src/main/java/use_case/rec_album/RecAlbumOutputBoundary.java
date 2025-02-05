a52802d96f0575b74fd7a85238cadd785edb715b
riena42 <riena.asakawa@mail.utoronto.ca>
2025-02-04 19:40:20 -0500

    Update RecAlbumOutputBoundary.java
    pasted output boundary

--- src/main/java/use_case/rec_album/RecAlbumOutputBoundary.java
+++ src/main/java/use_case/rec_album/RecAlbumOutputBoundary.java
@@ -1,4 +1,20 @@
 package use_case.rec_album;
 
+/**
+ * Output boundary for recommending an album.
+ */
 public interface RecAlbumOutputBoundary {
+    /**
+     * Prepares the success view.
+     *
+     * @param recAlbumOutputData Output data for the use case.
+     */
+    void prepareSuccessView(RecAlbumOutputData recAlbumOutputData);
+
+    /**
+     * Prepares the failure view.
+     *
+     * @param recAlbumOutputData Output data for the use case.
+     */
+    void prepareFailView(RecAlbumOutputData recAlbumOutputData);
 }
