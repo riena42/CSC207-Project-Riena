6a3d58ebbe20d9e95d761ac9557e7aba87a66299
riena42 <riena.asakawa@mail.utoronto.ca>
2025-02-04 19:39:36 -0500

    Update RecAlbumInputData.java
    pasted input data

--- src/main/java/use_case/rec_album/RecAlbumInputData.java
+++ src/main/java/use_case/rec_album/RecAlbumInputData.java
@@ -1,4 +1,46 @@
 package use_case.rec_album;
 
+import java.util.ArrayList;
+
+/**
+ * Input data for recommending an album.
+ */
 public class RecAlbumInputData {
+
+    private final String id;
+    private final String name;
+    private final ArrayList<String> artists;
+    @SuppressWarnings({"checkstyle:MemberName", "checkstyle:SuppressWarnings"})
+    private final String album_type;
+    private final ArrayList<Object> images;
+
+    public RecAlbumInputData(String id, String name,
+                             ArrayList<String> artists, String album_type, ArrayList<Object> images) {
+        this.id = id;
+        this.name = name;
+        this.artists = artists;
+        this.album_type = album_type;
+        this.images = images;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getArtists() {
+        return artists;
+    }
+
+    public String getAlbumType() {
+        return album_type;
+    }
+
+    public ArrayList<Object> getImages() {
+        return images;
+    }
+
 }
